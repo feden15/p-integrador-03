dec2c76ce3cdef39d2475c57591473fa6951602d
Federico Nova <fedenova8@gmail.com>
2025-05-03 20:32:59 -0300

    Creamos un esquema para los productos a cargar

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 const app = express()
 const PORT = 8080
 
+// ! Conexión de base de datos
+
 const conexionDB = async () => {
     try {
         await mongoose.connect(process.env.DB_URI)
@@ -13,12 +15,47 @@ const conexionDB = async () => {
     }
 }
 
+// ? 1. Crear un ESQUEMA
+// Para crear un SCHEMA lo que hago es decirle a Mongoose cómo va a ser el documento (qué forma va a tener)
+
+const productoSchema = new mongoose.Schema(
+    {
+        nombre: String,
+        precio: Number,
+        stock: Number,
+        marca: String,
+        categoria: String,
+        detalles: String,
+        foto: String,
+        envio: Boolean
+    }
+)
+
+// ? 2. Crear un MODELO (a partir del esquema)
+// Le digo a Mongoose que el documento descripto en el esquema, se va a guardar en la colección indicada
+
+//                             ('nombre-colección', schema a usar)
+const ProductoModelo = mongoose.model('productos', productoSchema)
+
+// ! --------------------------------------------------------------------------
+
+// ! Middlewares
+// ! --------------------------------------------------------------------------
+
+// ! Rutas
+
 app.get('/', (req, res) => {
     res.send('Proyecto Integrador - Etapa 3')
 })
 
+// ! --------------------------------------------------------------------------
+
+// ! Arranque
+
 app.listen(PORT, (err) => {
     if (err) throw new Error('No se pudo levantar el servidor')
-    console.log(`Aplicación funcionando http://localhost:${PORT}`)
+        console.log(`Aplicación funcionando http://localhost:${PORT}`)
     conexionDB()
 })
+
+// ! --------------------------------------------------------------------------
