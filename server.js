091e9392ed46c0d6fe93e39770ac69bb6d410d06
Federico Nova <fedenova8@gmail.com>
2025-05-05 10:31:35 -0300

    Instalamos cors. Le dimos acceso a la carpeta public, solo a nuestro front

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ import connection from './utils/connection.js'
 import routerProductos from './routers/productos.router.js'
 import routerUploads from './routers/uploads.router.js'
 import path from 'node:path'
+import cors from 'cors'
+
+// ! Constantes
 
 const app = express()
 const PORT = 8080
 const URI_DB = process.env.URI_LOCAL
+const URL_FRONT = process.env.URL_FRONTEND_CORS
+// ! --------------------------------------------------------------------------
+
+// ! Configuraciones
+
+const corsConfig = {
+    origin: URL_FRONT // link completo de netlify (frontend)
+}
+// ! --------------------------------------------------------------------------
 
 // ! Middlewares
 
 app.use(express.json()) // para poder comprender lo que llega en el body a través de un json
 app.use(express.static(path.join('public')))
+app.use(cors(corsConfig))
 // ! --------------------------------------------------------------------------
 
 // ! Rutas
@@ -23,7 +36,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/productos', routerProductos)
 app.use('/api/v1/uploads/', routerUploads)
-
 // ! --------------------------------------------------------------------------
 
 // ! Arranque
@@ -33,5 +45,4 @@ app.listen(PORT, (err) => {
         console.log(`Aplicación funcionando http://localhost:${PORT}`)
     connection(URI_DB)
 })
-
 // ! --------------------------------------------------------------------------
