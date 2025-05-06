afa589333a4e680b935814dc968a149f5e42ddea
Federico Nova <fedenova8@gmail.com>
2025-05-06 14:46:37 -0300

    Probando corss

--- server.js
+++ server.js
@@ -13,54 +13,21 @@ const app = express()
 const PORT = process.env.PORT
 // const URI_DB = process.env.URI_LOCAL
 const URI_DB = process.env.URI_REMOTA
-// const URL_FRONT = process.env.URL_FRONTEND_CORS
+const URL_FRONT = process.env.URL_FRONTEND_CORS
 // ! --------------------------------------------------------------------------
 
 // ! Configuraciones
 
-// const corsConfig = {
-//     origin: function (origin, callback) {
-//         console.log('Origin recibido:', origin)
-//         if (!origin || origin === URL_FRONT) {
-//             console.log('Origin permitido')
-//             callback(null, true)
-//         } else {
-//             console.log('Origin bloqueado')
-//             callback(new Error('No permitido por CORS'))
-//         }
-//     }
-// }
-
-const allowedOrigins = [
-    process.env.URL_FRONTEND_CORS,
-    'http://localhost:3000' // Desarrollo
-];
+const corsConfig = {
+    origin: URL_FRONT
+}
 // ! --------------------------------------------------------------------------
 
 // ! Middlewares
 
 app.use(express.json()) // para poder comprender lo que llega en el body a través de un json
 app.use(express.static(path.join('public')))
-app.use(cors())
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    }
-    if (req.method === 'OPTIONS') return res.status(200).end();
-    next();
-});
-
-// Archivos estáticos con CORS específico
-app.use('/uploads',
-    (req, res, next) => {
-        res.header('Cross-Origin-Resource-Policy', 'cross-origin');
-        next();
-    },
-    express.static(path.join(process.cwd(), 'public', 'uploads'))
-);
+app.use(cors(corsConfig))
 // ! --------------------------------------------------------------------------
 
 // ! Rutas
