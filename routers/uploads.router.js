3590ca827b6d19024c0d381b6b6549fa877dd069
Federico Nova <fedenova8@gmail.com>
2025-05-07 21:27:17 -0300

    Creamos routerUploads.get para obtener las imágenes cargadas

--- routers/uploads.router.js
+++ routers/uploads.router.js
@@ -1,8 +1,25 @@
 import express from 'express'
+import path from 'node:path'
+import fs from 'node:fs'
+
 import controller from '../controllers/uploads.controller.js'
 import uploadsMiddleware from '../middlewares/uploads.middleware.js'
 
 const routerUploads = express.Router()
+
+
+routerUploads.get('/:filename', (req, res) => {
+    const filename = req.params.filename
+    const filePath = path.join(process.cwd(), 'public', 'uploads', filename)
+    // cwd te da la raíz -> public/uploads/imagen.jpg se ve como http://localhost:3000/uploads/imagen.jpg
+
+    // Verifica si el archivo existe
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({ mensaje: 'Archivo no encontrado' })
+    }
+
+    res.sendFile(filePath)
+})
 
 //                                                la key
 routerUploads.post('/', uploadsMiddleware.single('imagen'), controller.uploadImagen)
